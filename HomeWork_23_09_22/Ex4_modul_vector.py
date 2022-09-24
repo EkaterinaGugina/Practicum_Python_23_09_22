bf9355eae775d77ba02f4f26aacc310f476f8042
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-09-25 02:30:08 +0500

    Ex4 solved

--- HomeWork_23_09_22/Ex4_modul_vector.py
+++ HomeWork_23_09_22/Ex4_modul_vector.py
@@ -2,3 +2,8 @@
 # Пример:
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+from math import sqrt
+x_A, y_A = map(int, input('Введите координаты первой точки: ').split())
+x_B, y_B = map(int, input('Введите координаты второй точки: ').split())
+modul_vector = sqrt((x_A - x_B)**2 + (y_A - y_B)**2)
+print(f'A {x_A, y_A}; B {x_B, y_B} ---> АВ = {round(modul_vector, 2)}')
